60b03c7dac021e184279b60d4c031e25d414d8dd
JohnsonLu <johnson4932@gmail.com>
2013-09-13 10:17:15 +0000

    fix message error and finally createGroup

--- app.js
+++ app.js
@@ -89,7 +89,61 @@ app.post('/api/createVote', function(req,res) {
             res.send(JSON.stringify({Success : true, Result: result, Message: 'Create Vote Success'}));
         });
     } else {
-        res.send(JSON.stringify({Success : false, Message: 'Please inpu params'}));
+        res.send(JSON.stringify({Success : false, Message: 'Please input params'}));
+    }
+});
+
+app.post('/api/createGroup', function(req,res) {
+    res.setHeader('Content-Type', 'application/json');
+    var params = getParams(req);
+
+    if (params.clone) {
+        var sql = "SELECT `Title` FROM `vote_group` WHERE `VID`=?";
+        conn.db.query(sql, [params.cloneVote], function(err, rows, fiels) {
+            if (err) {
+                res.send(JSON.stringify({Success : false, Result: err, Message: 'Database Error'}));
+                return;
+            }
+            var insertSQL = "INSERT INTO `vote_group` VALUES";
+            var insertParams = [];
+            for (var key in rows) {
+                insertSQL += "(NULL,?,?),";
+                insertParams.push(params.VoteName);
+                insertParams.push(rows[key].Title);
+            }
+
+            conn.db.query(insertSQL.substr(0,insertSQL.length - 1), insertParams, function(err, result) {
+                if (err) {
+                    res.send(JSON.stringify({Success : false, Result: err, Message: 'Clone Group Fail, Database Error'}));
+                    return;
+                }
+                res.send(JSON.stringify({Success : true, Result: result, Message: 'Clone Group Success'}));
+            });
+        });
+    } else {
+        if (0 == params.GroupName.length) {
+            res.send(JSON.stringify({Success : false, Message: 'Please input params'}));
+            return;
+        }
+
+        var sql = "INSERT INTO `vote_group` VALUES";
+        var insertParams = [];
+        for(var key in params.GroupName) {
+            if ('' == params.GroupName[key]) {
+                continue;
+            }
+            sql += "(NULL,?,?),";
+            insertParams.push(params.VoteName);
+            insertParams.push(params.GroupName[key]);
+        }
+
+        conn.db.query(sql.substr(0,sql.length - 1), insertParams, function(err, result) {
+            if (err) {
+                res.send(JSON.stringify({Success : false, Result: err, Message: 'Create Group Fail, Database Error'}));
+                return;
+            }
+            res.send(JSON.stringify({Success : true, Result: result, Message: 'Create Group Success'}));
+        });
     }
 });
 
