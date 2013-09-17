8fa81401be8694b548008807e2a089ad112f7da8
JohnsonLu <johnson4932@gmail.com>
2013-09-17 02:48:25 +0000

    separate js

---
+++ public/js/admin.js
@@ -0,0 +1,46 @@
+(function($){
+    $('#alertMessage').hide();
+
+    //createVoteModal
+    $('#createVoteModal .submit').click(function() {
+        $.post('/api/createVote', $('#createVoteModal form').serialize())
+        .done(function(data) {
+            $('#createVoteModal').modal('hide');
+            var voteStatus = (true == data.Success) ? ('success') : ('danger');
+            $('#alertMessage').html(data.Message).attr('class','alert alert-' + voteStatus).show();
+        })
+        .fail(function(err) {
+            $('#createVoteModal').modal('hide');
+            $('#alertMessage').html('Please Login').attr('class','alert alert-danger').show();
+        });
+    });
+
+    //createGroupModal
+    $('#createGroupModal .clone').click(function() {
+        var check = $('#createGroupModal .clone:checked').val();
+        var div = $(this).parents().find('.cloneDiv');
+        (undefined == check) ? (div.hide()) : (div.show());
+    });
+
+    $('#createGroupModal .addColumn').click(function(e) {
+        e.preventDefault();
+        $('#createGroupModal .group-name .form-control:first')
+            .clone()
+            .val("")
+            .appendTo('#createGroupModal .group-name');
+    });
+
+    $('#createGroupModal .submit').click(function() {
+        $.post('/api/createGroup', $('#createGroupModal form').serialize())
+        .done(function(data) {
+            $('#createGroupModal').modal('hide');
+            var voteStatus = (true == data.Success) ? ('success') : ('danger');
+            $('#alertMessage').html(data.Message).attr('class','alert alert-' + voteStatus).show();
+        })
+        .fail(function(err) {
+            $('#createGroupModal').modal('hide');
+            $('#alertMessage').html('Please Login').attr('class','alert alert-danger').show();
+        });
+    });
+
+})(jQuery);
