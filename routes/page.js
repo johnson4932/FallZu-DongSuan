bc630db2d1475cc620a3b97702756ef72c02f128
JohnsonLu <johnson4932@gmail.com>
2013-09-17 08:53:21 +0000

    add vote list table

--- routes/page.js
+++ routes/page.js
@@ -12,36 +12,23 @@ exports.login = function(req,res) {
 };
 
 exports.vote = function(req,res) {
-    if ('kspaic' == req.params.vid) {
-        res.render('votes_admin');
-    } else {
-        res.render('votes');
+    var obj = {Login:false};
+    if (req.session.login && req.session.name) {
+        //obj.Login
     }
+    res.render('votes',obj);
 };
 
 exports.admin = function(req,res) {
     if ( undefined == req.session.login || undefined == req.session.name) {
         res.redirect('/login');
     } else {
-        var sql = "SELECT `vote_list`.*, `vote_group`.`VGID`,`vote_group`.`Title` FROM `vote_list` LEFT JOIN `vote_group` ON `vote_list`.`VID`=`vote_group`.`VID`";
+        var sql = "SELECT * FROM `vote_list` ORDER BY `VID` DESC";
         conn.db.query(sql, function(err, rows, fiels) {
-            var arr = new Object;
-            for (var key in rows) {
-                console.log(arr[rows[key].VID]);
-                if (undefined == arr[rows[key].VID]) {
-                    arr[rows[key].VID] = new Object;
-                    arr[rows[key].VID].Group = [];
-                }
-                arr[rows[key].VID].VoteName = rows[key].VoteName;
-                arr[rows[key].VID].VoteDate = rows[key].VoteDate;
-                arr[rows[key].VID].Group.push(rows[key].Title);
-            }
-            console.log(arr);
-
             res.render('admin',{
                 Account: req.session.login,
                 Name   : req.session.name,
-                Result : arr
+                Result : rows
             });
         });
     }
