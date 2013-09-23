43faf608402492b3392d19e9752ae07a63184766
JohnsonLu <johnson4932@gmail.com>
2013-09-23 10:38:37 +0000

    apply DB to modify.ejs

--- routes/page.js
+++ routes/page.js
@@ -1,4 +1,5 @@
-var conn    = require('../routes/mysql');
+var conn    = require('../routes/mysql'),
+    async   = require('async');
 exports.index = function(req,res) {
     if (req.session.login && req.session.name) {
         res.redirect('/admin');
@@ -35,7 +36,15 @@ exports.modify = function (req,res) {
     if ( undefined == req.session.login || undefined == req.session.name) {
         res.redirect('/login');
     } else {
-        res.render('modify',{VID:req.params.vid});
+        var VID = req.params.vid;
+        async.parallel([
+            function(callback){ conn.db.query("SELECT * FROM `vote_candidate` WHERE `VID`=?", [VID], function(err, result){ callback(err, result);}); },
+            function(callback){ conn.db.query("SELECT * FROM `vote_group` WHERE `VID`=?", [VID], function(err, result){ callback(err, result);}); },
+            function(callback){ conn.db.query("SELECT * FROM `vote_list` WHERE `VID`=? LIMIT 1", [VID], function(err, result){ callback(err, result);}); }
+        ], function(err, result) {
+            console.log(result[2]);
+            res.render('modify',{VID:VID, Candidate:result[0], Group:result[1], Vote:result[2]});
+        });
     }
 }
 
