8305b58f361b120c3f0eb99d61268e7557037edc
JohnsonLu <johnson4932@gmail.com>
2013-09-27 02:36:32 +0000

    add vote_list info

--- routes/page.js
+++ routes/page.js
@@ -17,12 +17,14 @@ exports.vote = function(req,res) {
     async.parallel([
         function(callback){ conn.db.query("SELECT `vote_candidate`.*,`vote_group`.`Title` FROM `vote_candidate` LEFT JOIN `vote_group` ON `vote_candidate`.`VGID` = `vote_group`.`VGID` WHERE `vote_candidate`.`VID`=?", [VID], function(err, result){ callback(err, result);}); },
         function(callback){ conn.db.query("SELECT * FROM `vote_group` WHERE `VID`=?", [VID], function(err, result){ callback(err, result);}); },
+        function(callback){ conn.db.query("SELECT * FROM `vote_list` WHERE `VID`=? LIMIT 1", [VID], function(err, result){ callback(err, result);}); },
     ],function(err, result) {
         var obj = {
             Login       :req.session.login,
             VID         :VID,
             Candidate   :result[0],
-            Group       :result[1]
+            Group       :result[1],
+            Vote        :result[2]
         };
         res.render('votes',obj);
     });
